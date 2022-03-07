47ad8b5913d9505a4d90b62f71fd914f6b4ec768
amzesmoro <amzesmoro05@gmail.com>
2022-03-07 17:14:00 +0700

    adding inheritance

--- src/main/kotlin/app/Inheritance.kt
+++ src/main/kotlin/app/Inheritance.kt
@@ -4,5 +4,9 @@ import data.Manager
 import data.VicePresident
 
 fun main() {
+    val manager = Manager("Abidin")
+    manager.sayHello("Yusuf")
 
+    val vicePresident = VicePresident("Ujang")
+    vicePresident.sayHello("Alex")
 }
