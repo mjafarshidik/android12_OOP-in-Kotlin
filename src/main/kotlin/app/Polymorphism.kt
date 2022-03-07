5e4858bf3ad759ea8ba541c8a6908cb06818c9e7
mjafarshidik <mjafarshidik86@gmail.com>
2022-03-07 16:31:21 +0700

    Added polymorphism

--- src/main/kotlin/app/Polymorphism.kt
+++ src/main/kotlin/app/Polymorphism.kt
@@ -5,4 +5,12 @@ import data.Manager
 import data.VicePresident
 
 fun main() {
+    var employee: Employee = Employee("Yusuf")
+    employee.sayHello("Sidik")
+
+    employee = Manager("Yolanda")
+    employee.sayHello("Rojak")
+
+    employee = VicePresident("Lukman")
+    employee.sayHello("Dimas")
 }
