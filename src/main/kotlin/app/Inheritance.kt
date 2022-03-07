e3b4751db63ee73d87ac1232189e2cfdf84430f5
amzesmoro_sianturi <amzesmoro@credibook.com>
2022-03-07 17:11:31 +0700

    Revert "add inheritance"
    This reverts commit 1446f7029eba001509fc653af48460ca74c7cee6.

--- src/main/kotlin/app/Inheritance.kt
+++ src/main/kotlin/app/Inheritance.kt
@@ -4,9 +4,5 @@ import data.Manager
 import data.VicePresident
 
 fun main() {
-    val manager = Manager("Abidin")
-    manager.sayHello("Yusuf")
 
-    val vicePresident = VicePresident("Ujang")
-    vicePresident.sayHello("Alex")
 }
